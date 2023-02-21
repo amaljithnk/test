6856b91469564121f32089c4f6bf331ffd8cc7c6
amaljithnk <amaljithnk457@gmail.com>
2023-02-21 17:30:43 +0530

    major revision

---
+++ test.py
@@ -0,0 +1,14 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arange(1,10)
+y=np.sin(x)
+plt.plot(y)
+plt.show()
+#%%
+s=np.arange(11,30)
+l=np.cos(s)
+plt.plot(l)
+plt.show()
+
+# %%
